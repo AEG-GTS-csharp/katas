8957aa18633d515fc46ddf237972ab117d0ab439
Mittelfeld55 <Mittelfeld55@gmail.com>
2025-12-12 16:04:55 +0100

    12.12

--- Session1/app/Program.cs
+++ Session1/app/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace App;
+﻿// HEHEHEHA
+namespace App;
 
 public class App
 {
@@ -11,20 +12,42 @@ public class App
         return 0;
     }
 
-    public static int CalculatePoints(string[] gameResult)
+    public static int CalculatePoints(String[] gameResult)
     {
         if (gameResult.Length != 10)
         {
             throw new ArgumentException(nameof(gameResult));
         }
+        foreach (string s in gameResult)
+        {
+            CheckFormat(s);
+        }
         return 0;
+
     }
 
     public static void CheckFormat(String gameResult)
     {
-        if (gameResult == String.Empty)
+        if (string.IsNullOrWhiteSpace(gameResult) || gameResult.Length > 3)
         {
             throw new FormatException();
         }
+        int colons = 0;
+        for (int i = 0; i < gameResult.Length; i++)
+        {
+            if (gameResult[i] == ':')
+            {
+                if (i != 1)
+                {
+                    throw new FormatException();
+                }
+                colons++;
+            }
+            else if (gameResult[i] > '4' || gameResult[i] < '1')
+            {
+                throw new FormatException();
+            }
+        }
+        if (colons != 1) throw new FormatException();
     }
 }
--- Session1/tests/UnitTest1.cs
+++ Session1/tests/UnitTest1.cs
@@ -17,14 +17,6 @@ public class Tests
     }
 
     [Test]
-    public void TakesStringArray()
-    {
-        string[] gameResult = new string[10];
-        int result = App.CalculatePoints(gameResult);
-        Assert.That(result == 0);
-    }
-
-    [Test]
     public void ThrowsOutOfRangeExIfLengthIsGreater11()
     {
         Random random = new Random();
@@ -52,10 +44,42 @@ public class Tests
     {
         Assert.Throws<FormatException>(() => App.CheckFormat(string.Empty));
     }
+
     [Test]
-    public void CheckFormat_IsResultGreaterThan3()
+    public void CheckFormat_IsResultLongerThan3()
     {
         Assert.Throws<FormatException>(() => App.CheckFormat("4:4:"));
     }
 
+    [Test]
+    [TestCase("4::")]
+    [TestCase(":::")]
+    [TestCase("::4")]
+    [TestCase("444")]
+    public void CheckFormat_ThrowsFormatExceptionIfNotExactlyOneColon(string result)
+    {
+        Assert.Throws<FormatException>(() => App.CheckFormat(result));
+    }
+
+    [Test]
+    [TestCase(":44")]
+    [TestCase("44:")]
+    public void CheckFormat_ThrowsFormatExceptionIfColonMislocated(string result)
+    {
+        Assert.Throws<FormatException>(() => App.CheckFormat(result));
+    }
+
+    [Test]
+    [TestCase("5:5")]
+    [TestCase("6:6")]
+    public void CheckFormat_ThrowsFormatExceptionIsScoreGreaterThanFour(string result)
+    {
+        Assert.Throws<FormatException>(() => App.CheckFormat(result));
+    }
+
+    [Test]
+    public void CalculatePointsChecksFormat()
+    {
+        Assert.Throws<FormatException>(() => App.CalculatePoints(["HEHEHEHA", "LOLa", "xD", "Tituuuus!", "Tituuuus!", "Tituuuus!", "Tituuuus!", "Tituuuus!", "Tituuuus!", "Tituuuus!",]));
+    }
 }
