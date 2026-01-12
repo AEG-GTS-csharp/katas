af69c78f8beaadd90f3a4629c48ba8609836323d
Finn Hackel <fhackel@netik.de>
2026-01-12 16:48:57 +0100

    Add project for session 2

---
+++ Session2/solution/Anagram.App/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Anagram.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string word = "documenting";
+            foreach (var (firstWord, secondWord) in FindTwoWordAnagramsOf(word))
+            {
+                Console.WriteLine($"'{firstWord}' and '{secondWord}' are two-word anagrams of '{word}'.");
+            }
+        }
+
+        public static List<string> LoadWordList()
+        {
+            using var file = File.OpenText("words.txt");
+
+            List<string> words = [];
+            while (true)
+            {
+                string? line = file.ReadLine();
+                if (line is null)
+                    break;
+
+                const int maxWordLength = 9;
+                for (int i = 0; i < line.Length / maxWordLength; i++)
+                {
+                    string word = line.Substring(i * maxWordLength, maxWordLength).TrimEnd();
+                    words.Add(word);
+                }
+            }
+
+            return words;
+        }
+
+        public static bool AreAnagrams(string firstWord, string secondWord)
+        {
+            char[] firstWordChars = firstWord.ToCharArray();
+            Array.Sort(firstWordChars);
+            char[] secondWordChars = secondWord.ToCharArray();
+            Array.Sort(secondWordChars);
+
+            return firstWordChars.SequenceEqual(secondWordChars);
+        }
+
+        public static List<(string, string)> FindTwoWordAnagramsOf(string word)
+        {
+            var wordList = LoadWordList();
+
+            List<(string, string)> anagrams = [];
+            foreach (string firstWord in wordList)
+            {
+                foreach (string secondWord in wordList)
+                {
+                    if (AreAnagrams(word, firstWord + secondWord))
+                    {
+                        anagrams.Add((firstWord, secondWord));
+                    }
+                }
+            }
+
+            return anagrams;
+        }
+    }
+}
---
+++ Session2/solution/Anagram.UnitTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Anagram.App;
+
+namespace Anagram.UnitTest
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void ReturnAllWordsShorterThenOrEqualTo9()
+        {
+            Assert.That(Program.LoadWordList().All(word => word.Length <= 9));
+        }
+
+        [Test]
+        public void ReturnAllWordsLongerThen0()
+        {
+            Assert.That(Program.LoadWordList().All(word => word.Length > 0));
+        }
+
+        [Test]
+        [TestCase("master", "stream")]
+        [TestCase("letters", "settler")]
+        [TestCase("turner", "return")]
+        public void ReturnTrueIfWordsAreAnagrams(string firstWord, string secondWord)
+        {
+            Assert.That(Program.AreAnagrams(firstWord, secondWord));
+        }
+
+        [Test]
+        [TestCase("master", "turner")]
+        [TestCase("letters", "example")]
+        [TestCase("they", "return")]
+        public void ReturnFalseIfWordsAreNotAnagrams(string firstWord, string secondWord)
+        {
+            Assert.That(!Program.AreAnagrams(firstWord, secondWord));
+        }
+
+        [Test]
+        public void CheckAnagramsForMegacity()
+        {
+            var anagrams = Program.FindTwoWordAnagramsOf("megacity");
+            Assert.That(
+                (anagrams.Contains(("mega", "city")) || anagrams.Contains(("city", "mega"))) && 
+                (anagrams.Contains(("game", "city")) || anagrams.Contains(("city", "game")))
+            );
+        }
+    }
+}
